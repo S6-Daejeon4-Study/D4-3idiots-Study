9433411adf3e76578ccf37b7f4ad60c9a56b9dad
Kim Jiun <54028476+rlajiun@users.noreply.github.com>
2021-12-27 22:51:13 +0900

    Update P42884_단속카메라_김지언.java

--- 11주차/프로그래머스 42884 단속카메라/P42884_단속카메라_김지언.java
+++ 11주차/프로그래머스 42884 단속카메라/P42884_단속카메라_김지언.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] routes) {
-        int answer = 0;
-        int cur = -30001;
-        
-        Arrays.sort(routes, new Comparator<int[]>() {
-        	@Override
-        	public int compare(int[] o1, int[] o2) {
-        		return o1[1] - o2[1];
-        	}
+	public int solution(int[][] routes) {
+		int answer = 0;
+		int cur = -30001;
+
+		Arrays.sort(routes, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] - o2[1];
+			}
 		});
 
-        for(int i = 0 ; i < routes.length; i++) {
-        	int[] route = routes[i];
-        	if(cur >= route[0])
-        		continue;
-        	cur = route[1];
-            answer++;
-        }
-        
-        return answer;
-    }
+		for (int i = 0; i < routes.length; i++) {
+			int[] route = routes[i];
+			if (cur >= route[0])
+				continue;
+			cur = route[1];
+			answer++;
+		}
+
+		return answer;
+	}
 }
