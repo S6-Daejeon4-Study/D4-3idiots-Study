a9ed9519e2cbc5cfc1d5c8591c2d2d217643e08c
김응철 <zbqlr456@naver.com>
2022-02-21 18:50:08 +0900

    김응철

---
+++ 17주차/프로그래머스 42626 더 맵게/P42626_더맵게_김응철.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0;i< scoville.length;i++){
+            pq.add(scoville[i]);
+        }
+        
+        while(pq.peek() <= K){
+            if(pq.size() == 1){
+                return -1;
+            }
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            int newScoville = first + (second*2);
+            
+            pq.add(newScoville);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
